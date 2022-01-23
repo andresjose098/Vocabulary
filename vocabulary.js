72c4258f2d9ddf4e34aab7d0284b7c4849d00c4e
andres <andres-jose1998@hotmail.com>
2022-01-22 23:37:09 -0500

    first commit

---
+++ vocabulary.js
@@ -0,0 +1,35 @@
+const CAT = () =>{
+
+ //   document.getElementById("imagesvocabularya").style.backgroundColor="blue";
+ document.getElementById("imagesvocabularya").style.backgroundImage = "url('images/CAT.JPG')";
+ var image= document.getElementById("imagesvocabularya");
+ image.style.width = "1200px";
+ image.style.height = "900px";
+
+
+}
+
+function DOG() {
+ //   document.getElementById("imagesvocabularya").style.backgroundColor = "red";
+    document.getElementById("imagesvocabularya").style.backgroundImage = "url('images/DOG.JPG')";
+ var DOG= document.getElementById("imagesvocabularya");
+ DOG.style.width = "1200px";
+ DOG.style.height= "900px";
+}
+
+function BIRD() {
+    //   document.getElementById("imagesvocabularya").style.backgroundColor = "red";
+       document.getElementById("imagesvocabularya").style.backgroundImage = "url('images/BIRD.JPG')";
+    var DOG= document.getElementById("imagesvocabularya");
+    DOG.style.width = "500px";
+    DOG.style.height= "500px";
+   }
+
+
+   function PYTHON() {
+    //   document.getElementById("imagesvocabularya").style.backgroundColor = "red";
+       document.getElementById("imagesvocabularya").style.backgroundImage = "url('images/PYTHON.JPG')";
+    var DOG= document.getElementById("imagesvocabularya");
+    DOG.style.width = "600px";
+    DOG.style.height= "400px";
+   }
