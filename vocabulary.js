ba9aeb040665d4e2b67869791ac8595e0cba8cac
andres <andres-jose1998@hotmail.com>
2022-01-23 22:07:07 -0500

    change

--- vocabulary.js
+++ vocabulary.js
@@ -1,7 +1,7 @@
 const CAT = () =>{
 
  //   document.getElementById("imagesvocabularya").style.backgroundColor="blue";
- document.getElementById("imagesvocabularya").style.backgroundImage = "url('images/CAT.JPG')";
+ document.getElementById("imagesvocabularya").style.backgroundImage = "url('Images/CAT.JPG')";
  var image= document.getElementById("imagesvocabularya");
  image.style.width = "1200px";
  image.style.height = "900px";
@@ -11,7 +11,7 @@ const CAT = () =>{
 
 function DOG() {
  //   document.getElementById("imagesvocabularya").style.backgroundColor = "red";
-    document.getElementById("imagesvocabularya").style.backgroundImage = "url('images/DOG.JPG')";
+    document.getElementById("imagesvocabularya").style.backgroundImage = "url('Images/DOG.JPG')";
  var DOG= document.getElementById("imagesvocabularya");
  DOG.style.width = "1200px";
  DOG.style.height= "900px";
@@ -19,7 +19,7 @@ function DOG() {
 
 function BIRD() {
     //   document.getElementById("imagesvocabularya").style.backgroundColor = "red";
-       document.getElementById("imagesvocabularya").style.backgroundImage = "url('images/BIRD.JPG')";
+       document.getElementById("imagesvocabularya").style.backgroundImage = "url('Images/BIRD.JPG')";
     var DOG= document.getElementById("imagesvocabularya");
     DOG.style.width = "500px";
     DOG.style.height= "500px";
@@ -28,7 +28,7 @@ function BIRD() {
 
    function PYTHON() {
     //   document.getElementById("imagesvocabularya").style.backgroundColor = "red";
-       document.getElementById("imagesvocabularya").style.backgroundImage = "url('images/PYTHON.JPG')";
+       document.getElementById("imagesvocabularya").style.backgroundImage = "url('Images/PYTHON.JPG')";
     var DOG= document.getElementById("imagesvocabularya");
     DOG.style.width = "600px";
     DOG.style.height= "400px";
